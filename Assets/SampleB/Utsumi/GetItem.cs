3f192a42f0c381e5a56281e8611f9891e6e32f55
Utsumi <kouki.utumi@gmail.com>
2021-06-19 16:55:18 +0900

    AddFunction
    アイテムに触れた時にアイテムが消えて船に現れる機能を追加したが、プレイヤーがうまくアイテムに触れないことや、取った後暫くすると、何故か走れなくなる不具合が発生。

---
+++ Assets/SampleB/Utsumi/GetItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetItem : MonoBehaviour
+{
+    
+    public GameObject[] VewObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "KeyItem") {
+            Debug.Log("Itemを認識");
+            VewObj[collision.gameObject.GetComponent<Item>().ItemNumber].SetActive(true);
+            Destroy(collision.gameObject);
+            
+        }
+    }
+}
