e3029fb33f73404e6a81f7ad347c2f5a9ae87775
SougoOmata <sougo.gamba.20001204@gmail.com>
2021-06-25 19:34:25 +0900

    update(O)
    活動時間内の制作

---
+++ Assets/sample/itemcheck.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemcheck : MonoBehaviour
+{
+    public GameObject[] itemobj;
+    string check;
+    System.Text.StringBuilder sb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        check = PlayerPrefs.GetString("objcheck","nnnnnnnnnn");
+        sb = new System.Text.StringBuilder(check);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i=0;i<12;i++)
+        {
+            if (itemobj[i].activeInHierarchy) sb.Replace("n","y",i,1);
+            Debug.Log(sb.ToString());
+        }
+    }
+}
