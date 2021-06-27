69a5a6578e946c1609f30f355a67855b7c25a0f3
Utsumi <kouki.utumi@gmail.com>
2021-06-27 19:12:21 +0900

    AddOP
    オープニングの試作品を追加

---
+++ Assets/SampleB/Utsumi/OPED/OP.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OP : MonoBehaviour
+{
+    public GameObject Order;
+    //private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //animator = GetComponent<Animator>();
+        StartCoroutine(enumerator());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator enumerator() {
+        yield return new WaitForSeconds(17);
+        Order.SetActive(true);
+    }
+    public void StartGame() {
+        SceneManager.LoadScene("stage");
+    }
+}
