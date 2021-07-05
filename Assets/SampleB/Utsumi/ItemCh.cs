6a1d4acbe515814374a1cd4ca3b8565b8e7b73eb
Utsumi <kouki.utumi@gmail.com>
2021-07-05 18:49:00 +0900

    Upda2
    EDにスクリプトを追加、城を改良以上

---
+++ Assets/SampleB/Utsumi/ItemCh.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCh : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    public static bool[] ch = new bool[12];
+    public GameObject[] go = new GameObject[ch.Length];
+    public void DelItem()
+    {
+        for (int i = 0; i < ch.Length; i++) {
+            ch[i] = false;
+        }
+    }
+    private void Start()
+    {
+        for (int i = 0; i < go.Length; i++) {
+            go[i].SetActive(ch[i]);
+        }
+    }
+}
