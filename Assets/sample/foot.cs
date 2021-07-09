f232ec86c654d56de8bf4dee3881eed43ae2a299
SougoOmata <sougo.gamba.20001204@gmail.com>
2021-07-09 19:51:23 +0900

    Up(O)

--- Assets/sample/foot.cs
+++ Assets/sample/foot.cs
@@ -8,6 +8,9 @@ public class foot : MonoBehaviour
     string footname;
     [SerializeField] AudioClip[] clips;
     private AudioSource audiosource;
+    private Terrain t;
+    private TerrainData tData;
+    private int groundIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,16 +28,35 @@ public class foot : MonoBehaviour
         switch (footname)
         {
             case "FULLO (Instance)":
-                audiosource.PlayOneShot(clips[0]);
+                audiosource.PlayOneShot(clips[0]);//wood
+                break;
+            case "FULLO.001 (Instance)":
+                audiosource.PlayOneShot(clips[1]);//nomal
                 break;
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        footmaterial = other.gameObject.GetComponent<Renderer>().material;
-        
-        if(!footmaterial.name.Equals("Default-Material (Instance)")) footname = footmaterial.name;
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.name.Equals("Trrain"))
+        {
+            // Terrainから現在地のAlphamapを取得する
+            Vector3 position = transform.position - t.transform.position;
+            int offsetX = (int)(tData.alphamapWidth * position.x / tData.size.x);
+            int offsetZ = (int)(tData.alphamapHeight * position.z / tData.size.z);
+            float[,,] alphamaps = tData.GetAlphamaps(offsetX, offsetZ, 1, 1);
+
+            // Alphamap中で成分が最大のTerrainLayerを探す
+            float[] weights = alphamaps.Cast<float>().ToArray();
+            int terrainLayer = System.Array.IndexOf(weights, weights.Max());
+            groundIndex = tagToIndex[terrainLayerToTag[terrainLayer]];
+
+            
+        }
+        else footmaterial = other.gameObject.GetComponent<Renderer>().material;
+
+        if (!footmaterial.name.Equals("Default-Material (Instance)")) footname = footmaterial.name;
         Debug.Log("footmaterial:" + footname);
     }
 }
