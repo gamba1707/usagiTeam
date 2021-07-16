ac2a3a0ec79971e21126d44753881ed02d46bf79
SougoOmata <sougo.gamba.20001204@gmail.com>
2021-07-16 11:55:04 +0900

    Last3

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -35,7 +35,6 @@ public class Player : MonoBehaviour
         gamesec += Time.deltaTime;
         x = Input.GetAxis("Horizontal");    //左右矢印キーの値(-1.0~1.0)
         z = Input.GetAxis("Vertical");//上下矢印キーの値(-1.0~1.0)
-        transform
         if (!ButtonManager.pause)
         {
             dash();
